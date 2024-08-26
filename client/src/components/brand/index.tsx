e4bced87202d9e7fa020052350056dc01cbf3031
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-08-26 16:57:27 -0300

    refactor: remove title from Brand

--- client/src/components/brand/index.tsx
+++ client/src/components/brand/index.tsx
@@ -5,7 +5,6 @@ export const Brand = () => {
   return (
     <Link
       to="/"
-      title="Logo"
       className="brand text-cyan-500 flex items-center transition duration-200 ease-in text-3xl font-bold"
     >
       Thunder
